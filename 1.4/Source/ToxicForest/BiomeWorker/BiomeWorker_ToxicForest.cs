8c3c6aef10b65837fe7f634dfbdf6bdbee0aefca
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-03-15 23:24:59 +0000

    Initial commit

---
+++ 1.4/Source/ToxicForest/BiomeWorker/BiomeWorker_ToxicForest.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace ToxicForest
+{
+    public class BiomeWorker_ToxicForest : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+            if (tile.WaterCovered) { return -100f; }
+            if (tile.temperature < 10f || tile.temperature > 17f) { return 0f; }
+            if (tile.rainfall < 1500f || tile.rainfall > 2100f) { return 0f; }
+            return 22f + (tile.temperature - 14f) * 1.6f + (tile.rainfall - 300f) / 172f;
+		}
+	}
+}
---
+++ 1.4/Source/ToxicForest/ToxicForestMod.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace ToxicForest
+{
+    public class ToxicForestMod : Mod
+    {
+        public static ToxicForestMod mod;
+
+        public static string VersionDir => Path.Combine(mod.Content.ModMetaData.RootDir.FullName, "Version.txt");
+
+        public static string CurrentVersion { get; private set; }
+
+        public ToxicForestMod(ModContentPack content) : base(content)
+        {
+            mod = this;
+
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            CurrentVersion = $"{version.Major}.{version.Minor}.{version.Build}";
+
+            LogUtil.LogMessage($"{CurrentVersion} ::");
+
+            File.WriteAllText(VersionDir, CurrentVersion);
+
+            Harmony harmony = new Harmony("Neronix17.ToxicForest.RimWorld");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
