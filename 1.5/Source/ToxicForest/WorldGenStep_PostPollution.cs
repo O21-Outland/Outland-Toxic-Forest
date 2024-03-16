703b0229093294c1fbd3a959309f02814293be14
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-03-16 02:45:31 +0000

    1.5 stuff

---
+++ 1.5/Source/ToxicForest/WorldGenStep_PostPollution.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace ToxicForest
+{
+    public class WorldGenStep_PostPollution : WorldGenStep
+    {
+        public override int SeedPart => 759372056;
+
+        public override void GenerateFresh(string seed)
+        {
+            WorldGrid worldGrid = Find.WorldGrid;
+            for (int i = 0; i < worldGrid.TilesCount; i++)
+            {
+                Tile tile = worldGrid.tiles[i];
+                if (tile.biome.HasModExtension<DefModExt_ToxifyBiome>())
+                {
+                    WorldPollutionUtility.PolluteWorldAtTile(i, new FloatRange(0.3f, 1.5f).RandomInRange);
+                }
+            }
+        }
+    }
+}
