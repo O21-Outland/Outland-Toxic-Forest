93a0a2e8662738ae5de5bd10853328a6760c39b6
Neronix17 <39186138+neronix17@users.noreply.github.com>
2025-07-26 20:57:43 +0100

    1.6.9338

---
+++ 1.6/Source/ToxicForest/Patch_WorldGenStep_Pollution.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using HarmonyLib;
+using RimWorld.Planet;
+
+namespace ToxicForest
+{
+    [HarmonyPatch(typeof(WorldGenStep_Pollution), "GenerateFresh")]
+    public static class Patch_WorldGenStep_Pollution
+    {
+        [HarmonyPostfix]
+        public static void Postfix(string seed, PlanetLayer layer)
+        {
+            for (int i = 0; i < layer.TilesCount; i++)
+            {
+                Tile tile = layer[i];
+                if (tile.PrimaryBiome.HasModExtension<DefModExt_ToxifyBiome>())
+                {
+                    tile.pollution = 1f;
+                }
+            }
+        }
+    }
+}
